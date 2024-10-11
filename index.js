ee3876990d0721cc2ca8e161061f3b28a149d89c
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-11 08:15:52 +0500

    4--oy 7--dars

---
+++ index.js
@@ -0,0 +1,184 @@
+//1-masala 
+//Musbat va manfiy sonni tekshirish
+//function m(son) {
+//    return son > 0;
+//}
+//console.log(m(12));
+//2-masala 
+//Ikki sonni solishtirish
+//function t(a, b) {
+//    return a === b;
+//}
+//console.log(t);
+//3-masala 
+//Toq yoki juft sonni aniqlash
+//function j(son) {
+//    return son % 2 === 0;
+//}
+//console.log(j(12));
+//
+//4-masala 
+//10 dan kichik sonlarni tekshirish
+//function k(son) {
+//    return son < 10;
+//}
+//console.log(k(13));
+//
+//5-masala 
+//Uchburchak shaklini yasash
+//
+//function uchb(a, b, c) {
+//    return a + b > c && a + c > b && b + c > a;
+//}
+//console.log(uchb(12,13,56));
+//6-masala
+//Kvadrat sonni hisoblash
+//function kv(son) {
+//    return son * son;
+//}
+//console.log(kv());
+//7. 
+//Maksimum qiymatni topish
+//
+//function ma(a, b) {
+//    return Math.max(a, b);
+//}
+//console.log(ma());
+//8-masala
+// Raqamning musbatligini tekshirish
+//function m(son) {
+//    return son > 0;
+//}
+//console.log(m);
+//9-masala 
+//Kvadrat ildiz olish
+//function i(son) {
+//    return Math.sqrt(son);
+//}
+//console.log(i(12));
+//11-masala
+// Sonlar orasidan minimumni topish
+//function mn(a, b) {
+//    return Math.min(a, b);
+//}
+//console.log(mn(12,32));
+//12-masala
+// Ikki sonning ko'paytmasi
+//
+//function k(a, b) {
+//    return a * b;
+//}
+//console.log(k);
+//
+//13-masala
+// Bo'lish amali
+//
+//function b(son, n) {
+//    return son / n;
+//}
+//console.log(b(12,15));
+//
+//14-masala
+// Tenglikni tekshirish
+//
+//function t(a, b) {
+//    return a === b;
+//}
+//console.log(t(12,3));
+//
+//15-masala
+// Sonni 2 ga bo'lish
+//
+//function b2(son) {
+//    return son / 2;
+//}
+//console.log(b2(12));
+//
+//16-masala
+// Ikkita sonning yig'indisi
+//function y(a, b) {
+//    return a + b;
+//}
+//console.log(y);
+//
+//17-masala 
+//Ikkita sonning ayirmasi
+//
+//function ayirma(a, b) {
+//    return a - b;
+//}
+//console.log(ayirma(12,13));
+//
+//18-masala 
+//Yig'indini hisoblash
+//function y(son) {
+//    let sum = 0;
+//    for (let i = 1; i <= son; i++) {
+//        sum += i;
+//    }
+//    return sum;
+//}
+//console.log(y(132));
+//19-masala
+//Juft sonlarni yig'ish
+//function jy(son) {
+//    let sum = 0;
+//    for (let i = 2; i <= son; i += 2) {
+//        sum += i;
+//    }
+//    return sum;
+//}
+//console.log(jy(13));
+//
+//20-masala 
+//Sonni raqamlarini qo'shish
+//function ry(son) {
+//    let sum = 0;
+//    while (son !== 0) {
+//        sum += son % 10;
+//        son = Math.floor(son / 10);
+//    }
+//    return sum;
+//}
+//console.log(ry(12));
+//
+//22-masala 
+//Sonning negativini qaytarish
+//function n(son) {
+//    return -son;
+//}
+//console.log(n(12));
+//
+//23-masla 
+//Butun son hisoblash
+//function bs(son) {
+//    return Math.floor(son);
+//}
+//console.log(bs(13));
+
+//24-masla 
+// sonni kubini hisoblash
+//function k(son) {
+//    return son * son * son;
+//}
+//console.log(k(12));
+//25-masala 
+//5 ga bo'linishi tekshirish
+//function b(son) {
+//    return son % 5 === 0;
+//}
+//console.log(b(12));
+
+//26-masala 
+//Sonning toq yoki juftligi tekshirish
+//function j(son) {
+//    return son % 2 ===0;
+//}
+//console.log(j(15));
+
+//27-masala 
+//2 sonning solishtirish
+//function t(x, y) {
+//    return x === y;
+//}
+//console.log(t(12,3));
